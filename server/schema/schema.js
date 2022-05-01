196246f954206aa8ad2f8f7be73388b419de9097
Miles Morlock <milesmorlockcode@gmail.com>
2022-05-01 15:04:17 -0400

    Created first part of the Book schema in graphql

---
+++ server/schema/schema.js
@@ -0,0 +1,12 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    field: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString}
+    })
+});
